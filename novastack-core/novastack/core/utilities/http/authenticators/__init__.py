583d67fa041aa7e391c6f85da4571805c7ee054f
Leonardo Furnielis <leonardofurnielis@outlook.com>
2026-04-16 14:45:50 -0300

    add import

--- novastack-core/novastack/core/utilities/http/authenticators/__init__.py
+++ novastack-core/novastack/core/utilities/http/authenticators/__init__.py
@@ -2,6 +2,9 @@ from novastack.core.utilities.http.authenticators.base import BaseAuthenticator
 from novastack.core.utilities.http.authenticators.basic_authenticator import (
     BasicAuthenticator,
 )
+from novastack.core.utilities.http.authenticators.ibm_iam_authenticator import (
+    IBMIAMAuthenticator,
+)
 from novastack.core.utilities.http.authenticators.no_auth_authenticator import (
     NoAuthAuthenticator,
 )
@@ -13,6 +16,7 @@ from novastack.core.utilities.http.authenticators.oauth2_authenticator import (
 __all__ = [
     "BaseAuthenticator",
     "BasicAuthenticator",
+    "IBMIAMAuthenticator",
     "NoAuthAuthenticator",
     "OAuth2Authenticator",
     "OAuth2GrantType",
