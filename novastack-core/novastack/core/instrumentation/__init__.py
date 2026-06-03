f13203e36c8e1c369ee35ed63e1804e105b48351
Leonardo Furnielis <leonardofurnielis@outlook.com>
2026-06-03 01:45:26 -0300

    build: add common and instrumentation to core

---
+++ novastack-core/novastack/core/instrumentation/__init__.py
@@ -0,0 +1,11 @@
+from novastack_instrumentation import (
+    DispatcherSpanMixin,
+    SpanExceptionEvent,
+    get_dispatcher,
+)
+
+__all__ = [
+    "DispatcherSpanMixin",
+    "get_dispatcher",
+    "SpanExceptionEvent",
+]
---
+++ novastack-core/novastack/core/instrumentation/span/__init__.py
@@ -0,0 +1,3 @@
+from novastack_instrumentation.span import BaseSpan, Span
+
+__all__ = ["BaseSpan", "Span"]
