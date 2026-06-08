8d8c5ccdaa8c6eebfcc9edc8409bf3d97e8be88f
Leonardo Furnielis <leonardofurnielis@outlook.com>
2026-06-08 11:17:15 -0300

    feat: add telemetry to retrievers and vectordb

--- novastack-core/novastack/core/retrievers/base.py
+++ novastack-core/novastack/core/retrievers/base.py
@@ -1,11 +1,18 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from typing import Any
 
 from novastack.core.bridge.pydantic import BaseModel
 from novastack.core.document import DocumentWithScore
+from novastack.core.telemetry import DispatcherSpanMixin, get_dispatcher
+from novastack.core.telemetry.events.retrieval import (
+    RetrievalEndEvent,
+    RetrievalStartEvent,
+)
+
+dispatcher = get_dispatcher(__name__)
 
 
-class BaseRetriever(BaseModel, ABC):
+class BaseRetriever(BaseModel, DispatcherSpanMixin):
     """
     Abstract base class for document retrievers.
     """
@@ -13,7 +20,7 @@ class BaseRetriever(BaseModel, ABC):
     model_config = {"arbitrary_types_allowed": True}
 
     @abstractmethod
-    def query_documents(
+    def _query_documents(
         self,
         query: str,
         **kwargs: Any,
@@ -22,5 +29,31 @@ class BaseRetriever(BaseModel, ABC):
         Query and retrieve relevant documents.
         """
         raise NotImplementedError(
-            f"{self.__class__.__name__} must implement the query_documents() method"
+            f"{self.__class__.__name__} must implement the _query_documents() method"
         )
+
+    @dispatcher.span
+    def query_documents(
+        self,
+        query: str,
+        **kwargs: Any,
+    ) -> list[DocumentWithScore]:
+        """
+        Query and retrieve relevant documents.
+        """
+        config_dict = self.model_dump(exclude={"api_key"})
+        dispatcher.event(
+            RetrievalStartEvent(
+                query=query,
+                config_dict=config_dict,
+            )
+        )
+
+        documents = self._query_documents(query, **kwargs)
+
+        dispatcher.event(
+            RetrievalEndEvent(
+                documents=documents,
+            )
+        )
+        return documents
